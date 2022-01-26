ad5607948755321bab0d8914e8cd3b1d0d4bb3ae
patrikstaaf <staaf.patrik@gmail.com>
2022-01-26 20:10:00 +0100

    Test to add PHP CodeSniffer

--- config.php
+++ config.php
@@ -2,14 +2,6 @@
 
 declare(strict_types=1);
 
-// return [
-//     'driver' => 'mysql',
-//     'host' => '127.0.0.1',
-//     'database' => 'pokemon',
-//     'username' => 'root',
-//     'password' => '1991',
-// ];
-
 return [
     'driver' => $_ENV['DATABASE_DRIVER'],
     'host' => $_ENV['DATABASE_HOST'],
